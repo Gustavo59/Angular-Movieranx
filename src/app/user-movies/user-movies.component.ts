57394b1e78ba9f660f390cb07a0f4eaa6d23cde3
Gustavo59 <gustavosbarros59@gmail.com>
2020-09-26 17:47:47 -0300

    Rating do Filme do Usuario Na Lista de Filmes Avaliados Por Ele

--- src/app/user-movies/user-movies.component.ts
+++ src/app/user-movies/user-movies.component.ts
@@ -4,6 +4,7 @@ import { MoviesService } from '../service/movies.service';
 import { UserMoviesService } from '../service/user-movies.service';
 import { SessionService } from '../service/session.service';
 import * as $ from 'jquery';
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-user-movies',
@@ -13,34 +14,45 @@ import * as $ from 'jquery';
 export class UserMoviesComponent implements OnInit {
 
   filmes: Movie[] = [];
+  ratings = [];
   page = 1;
   pageSize = 16;
   username = localStorage.getItem("username");
+
+  currentRate = 3.5;
 
   constructor(
     private moviesService: MoviesService,
     private userMoviesService: UserMoviesService,
     private session: SessionService
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.userMoviesService.getAllWatchedMovies(this.session.userId)
       .subscribe(
         res => {
-          console.log(res)
+          console.log("RELACIONAMENTOS: ", res)
 
           var moviesId = res.map(p => (
             p.movieId
           ));
 
-          console.log(moviesId)
+          var moviesRatings = res.map(p => (
+            (p.rating / 2)
+          ));
+
+          this.ratings = moviesRatings;
+
+          console.log("MOVIES ID: ", moviesId)
+          console.log("RATINGS: ", this.ratings)
 
           this.moviesService.getAllWatchedMovies(moviesId)
             .subscribe(
               res => {
-                console.log(res)
-
                 this.filmes = res
+
+                console.log("FILMES: ", this.filmes)
               }, error => {
                 console.log(error)
               }
