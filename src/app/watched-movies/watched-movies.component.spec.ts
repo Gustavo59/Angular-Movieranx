45dc9ae449815be1ab89f915e19a495d193cddf8
Gustavo59 <gustavosbarros59@gmail.com>
2020-10-01 22:54:48 -0300

    Bug fix nome de componente

--- src/app/watched-movies/watched-movies.component.spec.ts
+++ src/app/watched-movies/watched-movies.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UserMoviesComponent } from './user-movies.component';
+import { WatchedMoviesComponent } from './watched-movies.component';
 
 describe('UserMoviesComponent', () => {
-  let component: UserMoviesComponent;
-  let fixture: ComponentFixture<UserMoviesComponent>;
+  let component: WatchedMoviesComponent;
+  let fixture: ComponentFixture<WatchedMoviesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserMoviesComponent ]
+      declarations: [WatchedMoviesComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserMoviesComponent);
+    fixture = TestBed.createComponent(WatchedMoviesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
