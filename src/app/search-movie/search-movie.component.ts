03179d9b47bcfaada01f5395c54ca56297362dbe
Gustavo59 <gustavosbarros59@gmail.com>
2020-10-06 20:33:04 -0300

    Ajustando design do retorno da busca

--- src/app/search-movie/search-movie.component.ts
+++ src/app/search-movie/search-movie.component.ts
@@ -13,8 +13,9 @@ import { MoviesService } from '../service/movies.service';
 export class SearchMovieComponent implements OnInit {
   filmes: Movie[] = [];
   page = 1;
-  pageSize = 16;
+  pageSize = 5;
   public innerWidth: any;
+  movieSearched;
 
   constructor(
     private moviesService: MoviesService,
@@ -23,48 +24,30 @@ export class SearchMovieComponent implements OnInit {
   ) {
   }
 
-
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.innerWidth = window.innerWidth;
-    if (this.innerWidth <= 1600 && this.innerWidth >= 1410) {
-      this.pageSize = 16;
-    } else if (this.innerWidth < 1025 && this.innerWidth >= 992) {
-      this.pageSize = 10;
-    } else if (this.innerWidth < 992 && this.innerWidth >= 768) {
-      this.pageSize = 18;
-    } else if (this.innerWidth < 768 && this.innerWidth >= 425) {
-      this.pageSize = 16;
-    } else if (this.innerWidth < 425 && this.innerWidth >= 375) {
-      this.pageSize = 16;
-    } else if (this.innerWidth < 375) {
-      this.pageSize = 12;
-    }
-  }
-  
-
   ngOnInit() {
     this.innerWidth = window.innerWidth;
-    this.route.queryParams.subscribe(params=>{
+    this.route.queryParams.subscribe(params => {
       let term = params.term
-      try{ 
+
+      this.movieSearched = term
+
+      try {
         term = term.replace(/ /g, "-")
         this.moviesService.findMoviesByTerm(term).then(
-        data =>{
-        console.log(data)
-        this.filmes = data
-        return data
-      })
-      }catch(e){
+          data => {
+            console.log(data)
+            this.filmes = data
+            return data
+          })
+      } catch (e) {
         console.log(e)
       }
 
     })
   }
 
-  async serchMovie(term:string) { 
-  
- 
+  async serchMovie(term: string) {
+
+
   }
 }
